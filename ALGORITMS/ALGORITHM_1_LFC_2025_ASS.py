8fdd7c4027730ccd5ffdbd2c46cd5692743fbbb7
AndresVelez31 <afveleza@eafit.edu.co>
2025-03-22 15:53:35 -0500

    algortihm 1 done

--- ALGORITMS/ALGORITHM_1_LFC_2025_ASS.py
+++ ALGORITMS/ALGORITHM_1_LFC_2025_ASS.py
@@ -12,19 +12,31 @@ def main():
         return
     
     valid_strings = set() #list in wich every string is unique
+    invalid_strings = set() #list in wich every string is unique
     
-    #
     while len(valid_strings) < num_strings:
-        valid_strings.add(generate_valid_string(random.randint(1,10))) #if the function generates a string that is already in the set, that one will be ignored and the function will generate a new one
+        valid_strings.add(generate_valid_string(random.randint(1,10)))
     
-    print("\nValid Strings:\n")
+    while len(invalid_strings) < num_strings:
+        invalid_strings.add(generate_invalid_string(random.randint(1,10))) #if the function generates a string that is already in the set, that one will be ignored and the function will generate a new one
+    
+    # We print in console the valid string and the in valid strings generated
+    print("\n --- Valid Strings: ---\n")
     for j in valid_strings:
         print(j)
         
-    # We save the valid strings in a txt file for it to be used in the next algorytmhs
+    print("\n --- InValid Strings: ---\n")
+    for j in invalid_strings:
+        print(j)
+        
+    # We save the valid and invalid strings in a txt file for it to be used in the next algorytmhs
+        
     with open("String.txt", "w") as out_file:
-        out_file.write("Valid strings:\n")
+        out_file.write("--- Valid Strings: ---\n")
         out_file.writelines(s + "\n" for s in valid_strings)
+        
+        out_file.write("\n --- InValid Strings: ---\n")
+        out_file.writelines(s + "\n" for s in invalid_strings)
     
 #In this function we create the string that belongs to the grammar: S → aSb | ε.
 def generate_valid_string(n):
@@ -32,5 +44,15 @@ def generate_valid_string(n):
         return ""
     return "a" + generate_valid_string(n - 1) + "b"
 
+#In this function we create the string that does not belong to the grammar: S → aSb | ε.
+def generate_invalid_string(n):
+    if n == 0:
+        return ""
+    return "a" + "b" + generate_valid_string(n - 1) + "a" + "b"
+
 if __name__ == "__main__":
     main()
+
+
+
+
