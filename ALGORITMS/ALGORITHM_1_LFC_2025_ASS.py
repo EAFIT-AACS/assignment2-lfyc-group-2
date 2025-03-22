97dbb417d7d2b2a18724cb717ebf71d3f56c506a
AndresVelez31 <afveleza@eafit.edu.co>
2025-03-22 14:43:56 -0500

    Valids Strings

--- ALGORITMS/ALGORITHM_1_LFC_2025_ASS.py
+++ ALGORITMS/ALGORITHM_1_LFC_2025_ASS.py
@@ -1,20 +1,7 @@
-def strings_into(n_string):
-    print(n_string)
+import random
 
-def main():
-    print("Welcome to the generator of strings!! How many strings would you like to create?")
-    
-    try:
-        num_strings = int(input())
-    except ValueError:
-        print("Please enter a valid integer.")
-        return
-    
-    strings_into(num_strings)
-    
-    # Abrimos un archivo txt en modo escritura en el que se cargarán las cadenas creadas
-    # with open("String.txt", "w") as out_file:
-    #     pass  # Aquí se puede escribir en el archivo si es necesario
-
-if __name__ == "__main__":
-    main()
+def generate_valid_string(n):
+    """Genera una cadena válida basada en la gramática S → aSb | ε."""
+    if n == 0:
+        return ""
+    return "a" + generate_valid_string(n - 1) + "b"
